cfd9bc21025cd385f10abce2566889edb7cd0c03
Chanel O'Reilly <chanel@shortfam.com>
2024-01-24 15:52:25 -0700

    first commit

---
+++ BorrowMyBookshelf/BorrowMyBookshelf.Server/Models/DatabaseConnector.cs
@@ -0,0 +1,38 @@
+﻿using MySql.Data.MySqlClient;
+using System.Data;
+
+namespace BorrowMyBookshelf.Server.Models
+{
+    public class DatabaseConnector
+    {
+        protected MySqlConnection GetConnection()
+        {
+            string connectionString = @"server=localhost;userid=root;password=1789;database=BorrowMyBookshelf";
+            MySqlConnection con = new MySqlConnection(connectionString);
+            return con;
+        }
+        public DataTable RunSqlCommand(string sqlQuery)
+        {
+            MySqlConnection con = GetConnection();
+
+            con.Open();
+            MySqlCommand command = new MySqlCommand(sqlQuery, con);
+            MySqlDataAdapter adapter = new MySqlDataAdapter(command);
+            DataTable dt = new DataTable();
+            adapter.Fill(dt);
+            con.Close();
+            return dt;
+        }
+
+        public string RunSqlCommandString(string sqlQuery)
+        {
+            MySqlConnection con = GetConnection();
+            int count;
+            con.Open();
+            MySqlCommand command = new MySqlCommand(sqlQuery, con);
+            count = Convert.ToInt32(command.ExecuteScalar());
+            con.Close();
+            return count.ToString();
+        }
+    }
+}
---
+++ BorrowMyBookshelf/BorrowMyBookshelf.Server/Models/UsersDatabase.cs
@@ -0,0 +1,13 @@
+﻿using MySql.Data.MySqlClient;
+using System.Data;
+
+namespace BorrowMyBookshelf.Server.Models
+{
+    public class UsersDatabase : DatabaseConnector
+    {
+        public string getUser(int id)
+        {
+            return "";
+        }
+    }
+}
